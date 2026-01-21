c01feb5414e03e79544dcfb24f0c197131e32143
EmaMaker <emamaker@tutanota.com>
2026-01-21 23:23:49 +0100

    save model at keyboard interrupt

--- MyMainVec.py
+++ MyMainVec.py
@@ -15,24 +15,20 @@ def main() -> None:
     
     vec_env = make_vec_env(MyWrapper.ISMPC2gym_env_wrapper, n_envs=8, env_kwargs={"verbose": False, "render": False}, vec_env_cls=SubprocVecEnv)
     vec_env = VecFrameStack(vec_env, n_stack=4)
-    # vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=True, clip_obs=100.0)
 
     model = PPO(NoBiasActionBiasACPolicy, vec_env, verbose=1, device="cpu", n_steps=32, ent_coef=0.05, n_epochs=2)
-
-    # i=4
-    model.load(f"ppo_hrp4_multienv0.zip")
-    # vec_env = VecNormalize.load(f"vec_normalized{i}.pkl", vec_env)
-    # model.load("ppo_hrp4_multienv1_checkpoint_reward95.zip")
-    # vec_env = VecNormalize.load("vec_normalized_checkpoint_reward95.pkl", vec_env)
+    # model = PPO.load(f"midsave.zip", env=vec_env)
     
     print("start training")
     new_logger = configure('./multi.log', ["stdout", "json", "log", "tensorboard"])
     model.set_logger(new_logger)
-    for i in range(1000):
-        model.learn(total_timesteps=8192)
-        model.save(f"ppo_hrp4_multienv{i%5}")
-        print(f"last save: ppo_hrp4_multienv{i%5}" + "@"*20)
+
+    try:
+        model.learn(total_timesteps=30000)
+    except KeyboardInterrupt:
+        model.save("midsave")
     print("end training")
+    model.save("midsave")
     
 
     # model = PPO.load("ppo_hrp4_multienv4_", env=env, device="cpu")
