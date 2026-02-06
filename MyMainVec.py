a1d978037a204646e5ba60328e258610fe90186c
EmaMaker <emamaker@tutanota.com>
2026-02-06 16:31:36 +0100

    main: parametrized model loading

--- MyMainVec.py
+++ MyMainVec.py
@@ -10,6 +10,7 @@ from MyPolicy import NoBiasActionBiasACPolicy
 import time
 import numpy as np
 import torch
+import MyPlotter
 
 def start_simulation(model_path: str, vecnorm_path: str):
     env = MyWrapper.ISMPC2gym_env_wrapper(verbose=False,render=True,frequency_change_grav=1)
@@ -30,15 +31,15 @@ def start_simulation(model_path: str, vecnorm_path: str):
     env.UpdatePlot()
     input("finished")
     
-def main(n_envs = 1, train = True, load = False, filename_model=f"ppo_hrp4_multienv_AM.zip", filename_env=f"vec_normalized_AM.pkl", desired_trajectory=100, footstep_scaler=0.9, catch_reference=False, action_decision = False) -> None:
+def main(n_envs = 1, train = True, load = False, filename_model=f"ppo_hrp4_multienv.zip", filename_env=f"vec_normalized.pkl", desired_trajectory=100, footstep_scaler=0.9, catch_reference=False, action_decision = False, curriculum_learning=False) -> None:
     
-    vec_env = make_vec_env(MyWrapper.ISMPC2gym_env_wrapper, n_envs, env_kwargs={"verbose": False, "render": n_envs == 1, "frequency_change_grav" : 1, "desired_trajectory": desired_trajectory, "footstep_scaler": footstep_scaler, "get_L_reference" : catch_reference,  "get_ref_node" : catch_reference, "action_decision": action_decision}, vec_env_cls=SubprocVecEnv)
+    vec_env = make_vec_env(MyWrapper.ISMPC2gym_env_wrapper, n_envs, env_kwargs={"verbose": False, "render": n_envs == 1, "frequency_change_grav" : 1, "desired_trajectory": desired_trajectory, "curriculum_learning": curriculum_learning, "footstep_scaler": footstep_scaler, "get_L_reference" : catch_reference,  "get_ref_node" : catch_reference, "action_decision": action_decision}, vec_env_cls=SubprocVecEnv)
     vec_env = VecFrameStack(vec_env, n_stack=4)
     
     # for new environment
     if load:
-        vec_env = VecNormalize.load("vec_normalized.pkl", vec_env)
-        model = PPO.load("ppo_hrp4_multienv", vec_env)
+        vec_env = VecNormalize.load(filename_env, vec_env)
+        model = PPO.load(filename_model, vec_env)
     else:
         vec_env = VecNormalize(vec_env, norm_obs=True, norm_reward=True, clip_obs=1000.0, clip_reward=50_000)
         model = PPO(NoBiasActionBiasACPolicy, vec_env, verbose=1, device="cpu", n_steps=64, ent_coef=0.01, learning_rate=1e-3, n_epochs=2)
@@ -81,10 +82,16 @@ def main(n_envs = 1, train = True, load = False, filename_model=f"ppo_hrp4_multi
         vec_env.training = True
         vec_env.norm_reward = True
         print("start training")
-        new_logger = configure('./multi.log', ["stdout", "json", "log", "tensorboard"])
+        new_logger = configure('./multi.log', ["stdout", "csv", "log"])
         model.set_logger(new_logger)
-        for i in range(1000):
-            model.learn(total_timesteps=2048*4)
+
+        try:
+            for i in range(1000):
+                model.learn(total_timesteps=2048*8)
+                model.save(filename_model)
+                vec_env.save(filename_env)
+                print(f"last save: ppo_hrp4_multienv" + " @"*20)
+        except KeyboardInterrupt:
             model.save(filename_model)
             vec_env.save(filename_env)
             print(f"last save: ppo_hrp4_multienv" + " @"*20)
@@ -93,4 +100,4 @@ def main(n_envs = 1, train = True, load = False, filename_model=f"ppo_hrp4_multi
     start_simulation(model_path=filename_model,vecnorm_path=filename_env)    
 
 if __name__ == "__main__":
-    main(n_envs=8)   
+    main(train=True, load=False, filename_model=f"ppo_hrp4_nextfootstep_curriculum2.zip", filename_env=f"ppo_hrp4_nextfootstep_curriculum2.pkl", curriculum_learning=True, n_envs=16, footstep_scaler=1., desired_trajectory=1)   
