b8aee5312b0e453d5f65593806fc068e816f5da5
EmaMaker <emamaker@tutanota.com>
2026-02-12 16:54:38 +0100

    MyMain: more parameters for easy plots

--- MyMain.py
+++ MyMain.py
@@ -12,18 +12,18 @@ from stable_baselines3.common.logger import Logger
 import MyLogger
 import MyPlotter
 
-def main(train = False, load = False, custom_action = True) -> None:
+def main(train = False, load = False, custom_action = True, filename_model="", filename_env="", footstep_scaler=0.9, desired_trajectory=100) -> None:
     
-    env = MyWrapper.ISMPC2gym_env_wrapper(verbose=False, render=True, max_step=500, frequency_change_grav=1, footstep_scaler=0., desired_trajectory=101)
+    env = MyWrapper.ISMPC2gym_env_wrapper(verbose=False, render=True, max_step=500, frequency_change_grav=1, footstep_scaler=footstep_scaler, desired_trajectory=desired_trajectory)
     env = DummyVecEnv([lambda: env])
     env = VecFrameStack(env, n_stack=4)
     
     if not load:
         env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=100.0, clip_reward=500)
-        model = PPO(NoBiasActionBiasACPolicy, env, verbose=1, device="cpu", n_steps=64, ent_coef=0.01, learning_rate=1e-3, n_epochs=2)
+        model = PPO(NoBiasActionBiasACPolicy, env, verbose=1, device="cpu", n_steps=64000, ent_coef=0.01, learning_rate=0., n_epochs=2)
     else:
-        env = VecNormalize.load("ppo_hrp4_nextfootstep_curriculum.pkl", env)
-        model = PPO.load("ppo_hrp4_nextfootstep_curriculum.zip", env)
+        env = VecNormalize.load(filename_env, env)
+        model = PPO.load(filename_model, env)
 
     new_logger = configure('./single.log', ["stdout", "log", "csv"])
     model.set_logger(new_logger)
@@ -33,9 +33,11 @@ def main(train = False, load = False, custom_action = True) -> None:
     if train:
         print("start training")
         for _ in range(10):
-            model.learn(total_timesteps=1024, callback=MyPlotter.PlotCallback())  
-            model.save('ppo_hrp4')
-            env.save("env_normalized.pkl")
+            # model.learn(total_timesteps=102400, callback=MyPlotter.PlotCallback(folder=f"nominal_plots_traj{desired_trajectory}_gravity_horizon100"))
+            # model.learn(total_timesteps=102400, callback=MyPlotter.PlotCallback(folder=f"{filename_model}_plots_traj{desired_trajectory}_gravity_horizon150_inclined"))
+            model.learn(total_timesteps=1024)
+            # model.save('ppo_hrp4')
+            # env.save("env_normalized.pkl")
             print('saved' + ' @'*20)
     
     
@@ -49,7 +51,7 @@ def main(train = False, load = False, custom_action = True) -> None:
         for _ in range(1500):
             
             if custom_action: action = np.array([[0.0, 0.0, 0.0]]) # send action just to make the robot going forward
-            else: action, _states = model.predict(s, deterministic=False)
+            else: action, _states = model.predict(s, deterministic=True)
             s, r, done, info = env.step(action)
 
             #if done: break
@@ -62,4 +64,4 @@ def main(train = False, load = False, custom_action = True) -> None:
 
 if __name__ == "__main__":
 
-    main(train = True, load = True, custom_action = True)
+    main(train = True, load = True, custom_action = True, filename_model="ppo_hrp4_scaled09_curriculum2_footdistance2.zip", filename_env="ppo_hrp4_scaled09_curriculum2_footdistance2.pkl", footstep_scaler=0.9, desired_trajectory=101)
